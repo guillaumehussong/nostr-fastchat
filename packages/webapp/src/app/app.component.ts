3a86b26c8283f930b163d7dc46c82b6818cd09d5
Guillaume H <66718486+guillaumehussong@users.noreply.github.com>
2023-06-02 17:26:23 +0200

    Upgraded Angular from v13 to v15.

--- packages/webapp/src/app/app.component.ts
+++ packages/webapp/src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ChatRelayMessage, SystemNotice, User } from '@websocket/types';
 import { AppService } from './app.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgSwitch, NgSwitchCase, NgFor } from '@angular/common';
 
 @Component({
-  selector: 'websocket-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'websocket-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchCase, MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule, NgFor, MatCardModule]
 })
 export class AppComponent implements OnInit {
   title = 'I am Nostr on Angular';
--- packages/webapp/src/main.ts
+++ packages/webapp/src/main.ts
@@ -1,13 +1,27 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppService } from './app/app.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, MatCardModule, MatInputModule, MatButtonModule, MatSnackBarModule, MatListModule),
+        AppService,
+        provideAnimations()
+    ]
+})
   .catch((err) => console.error(err));
